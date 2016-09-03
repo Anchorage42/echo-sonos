cdd477b9ff9994050620cbaff5c86cc5b7916b5a
Anchorage42 <a.j.michaud@gmail.com>
2016-09-02 22:02:10 -0600

    Create options.js

---
+++ lambda/src/options.js
@@ -0,0 +1,17 @@
+// If you setup basic auth in node-sonos-http-api's settings.json, change the username
+// and password here.  Otherwise, just leave this alone and it will work without auth.
+var auth = new Buffer("YOUR_USERNAME" + ":" + "YOUR_PASSWORD").toString("base64");
+
+var options = {
+  appid: "amzn1.echo-sdk-ams.app.amzn1.ask.skill.1a03c75b-a8f3-4f53-ad1e-4a4f0cabe3a5",
+  host: "ajmichaud.mynetgear.com",
+  port: "5005",
+  headers: {
+      'Authorization': 'Basic ' + auth,
+      'Content-Type': 'application/json'
+  },
+  useHttps: false, // Change to true if you setup node-sonos-http-api with HTTPS
+  rejectUnauthorized: true, // Change to false if you self-signed your certificate
+};
+
+module.exports = options;
